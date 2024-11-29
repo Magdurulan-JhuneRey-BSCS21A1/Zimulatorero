73e0232693bdb00bfa81ad5e3954d0802c975600
Magdurulan-JhuneRey-BSCS21A1 <c2014-23@itmlyceumalabang.onmicrosoft.com>
2024-11-29 13:14:35 +0800

    The Records

--- main.cpp
+++ main.cpp
@@ -2,24 +2,68 @@
 #include <iomanip>
 #include <chrono>
 #include <thread>
+#include <fstream>
+
+using namespace std;
+
+int hour = 0;
+int minute = 0;
+int day = 0;
+int current_minute = minute;
+void timeProcessor();
+void save();
+void load();
 
 int main() {
-    int hour = 0;
-    int minute = 0;
-
+    load();
+    thread timeThread(timeProcessor);
     while (true) {
-    
-        std::this_thread::sleep_for(std::chrono::milliseconds(833));
-        minute += 1;
+    	if(minute != current_minute){
+        cout << "Day: " << day << ", " << hour << ":" 
+             << setw(2) << setfill('0') << minute << "\n";
+        this_thread::sleep_for(chrono::milliseconds(500));
+        save();
+        current_minute = minute;
+    }
+    }
 
+    timeThread.join();
+    return 0;
+}
+
+void timeProcessor() {
+    while (true) {
+        this_thread::sleep_for(chrono::milliseconds(833));
+        minute += 1;
         if (minute >= 60) {
             hour += 1;
             minute = 0;
         }
-
-        std::cout << "Time: " << hour << ":" 
-                  << std::setw(2) << std::setfill('0') << minute << "\n";
+        if (hour >= 24) {
+            day += 1;
+            hour = 0;
+        }
     }
-
-    return 0;
 }
+
+void save() {
+    ofstream file("Zimulatorero.SAVE");
+    if (file.is_open()) {
+        file << "Days: " << day << "\n";
+        file << "Hours: " << hour << "\n";
+        file << "Minutes: " << minute << "\n";
+        file.close();
+    }
+}
+
+void load() {
+    ifstream file("Zimulatorero.SAVE");
+    if (file.is_open()) {
+        string label;
+        file >> label >> day;
+        file >> label >> hour;
+        file >> label >> minute;
+        file.close();
+    }
+}
+
